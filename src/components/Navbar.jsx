a5b4b185c005ebbc770cf717bafbc36f7d5e0e47
sangdil <b121050@iiit-bh.ac.in>
2023-08-12 01:26:37 +0530

    minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
+        <li ><a href="#" className='blueBtn'>Join Now</a></li>
       </ul>
     </nav>
   );
